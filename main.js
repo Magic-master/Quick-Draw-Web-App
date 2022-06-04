a6feede41769604792c199c6a1b49c0d7b4f2945
Magic-master <85842164+Magic-master@users.noreply.github.com>
2022-06-04 15:52:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,3 +26,43 @@ function setup() {
     background("white");
     canvas.mouseReleased(classifyCanvas);
 }
+
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    check_sketch();
+    if (drawn_sketch == sketch) {
+        answer_holder = "set";
+        score++;
+        document.getElementById('score').innerHTML = 'Score: ' + score;
+    }
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementbyId('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
+    document.getElementById('confidence').innerHTML = 'Confidence ' + Math.round(results[0].confidence * 100) + '%';
+}
+function check_sketch() {
+    timer_counter++;
+    document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
+    console.log(timer_counter);
+    if (timer_counter > 400) {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if (timer_check == "completed" || answer_holder == "set") {
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
